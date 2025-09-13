843a67bcc5b85969d6d8c7c1b702777c774010ed
Andrew87n <andlop@ukr.net>
2025-09-13 10:00:40 +0300

    added hw9

--- js/hw8.js
+++ js/hw8.js
@@ -1,21 +1,21 @@
-const friends2 = ["Mango", "Poly", "Kiwi", "Ajax",];
-let string = "";
-for (let i = 0; i < friends2.length; i += 1){
-    string += friends2[i] + ", ";
-}
-console.log(string );
+// const friends2 = ["Mango", "Poly", "Kiwi", "Ajax",];
+// let string = "";
+// for (let i = 0; i < friends2.length; i += 1){
+//     string += friends2[i] + ", ";
+// }
+// console.log(string );
 
-const friends = ["Mango", "Poly", "Kiwi", "Ajax",];
-console.log(friends.join(", "));
+// const friends = ["Mango", "Poly", "Kiwi", "Ajax",];
+// console.log(friends.join(", "));
 
-const cards = ["Карточка-1", "Карточка-2", "Карточка-3", "Карточка-4", "Карточка-5",];
-const copyCards = cards.slice();
-const cardToRemove = "Карточка-3";
-const cardToInsert = "Карточка-6";
-const cardToUpdate = "Карточка-4";
-copyCards.splice(2, 1);
-console.log(copyCards);
-copyCards.splice(4, 0, "Карточка-6");
-console.log(copyCards);
-copyCards.splice(2, 1, "Карточка-3+4",)
-console.log(copyCards);
+// const cards = ["Карточка-1", "Карточка-2", "Карточка-3", "Карточка-4", "Карточка-5",];
+// const copyCards = cards.slice();
+// const cardToRemove = "Карточка-3";
+// const cardToInsert = "Карточка-6";
+// const cardToUpdate = "Карточка-4";
+// copyCards.splice(2, 1);
+// console.log(copyCards);
+// copyCards.splice(4, 0, "Карточка-6");
+// console.log(copyCards);
+// copyCards.splice(2, 1, "Карточка-3+4",);
+// console.log(copyCards);
