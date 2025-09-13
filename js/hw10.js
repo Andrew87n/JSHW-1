ed1ac71188e06ef2f4b8a39b8f161994304fe6c4
Andrew87n <andlop@ukr.net>
2025-09-13 22:41:19 +0300

    added hw10

---
+++ js/hw10.js
@@ -0,0 +1,60 @@
+const numbers = [1, 5, 8, 12, 3, 15, 7, 20];
+const words = ["кіт", "собака", "миша", "папуга", "хомяк"];
+
+function countItems(array, condition){
+    let count = 0;
+    for (let i of array){
+        if (condition(i) === true){
+            count ++;
+        }
+    }
+    return count
+}
+
+const isEven = num => num % 2 === 0;
+const isLarge = num => num > 10;
+const isShort = word => word.length <= 3;
+
+console.log("Парних чисел:", countItems(numbers, isEven));
+console.log("Чисел більше 10:", countItems(numbers, isLarge));
+console.log("Коротких слів:", countItems(words, isShort));
+
+
+const calculate = (a, b, operation) => operation(a, b);
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => {
+    if (b !== 0){
+        return a / b;
+    } else {
+        return "error"
+    }
+}
+
+console.log(calculate(10, 5, add));
+console.log(calculate(10, 5, subtract));
+console.log(calculate(10, 5, multiply));
+console.log(calculate(10, 5, divide));
+console.log(calculate(10, 0, divide));
+
+
+
+const messagePlaceholder = message => {
+    message = prompt("Введіть повідомлення");
+    return message
+}
+
+function repeatMessage(times, messageCreator){
+    let newMessage = "";
+    for (let i = 0; i <= times - 1; i ++){
+       newMessage = messageCreator(i);
+       console.log(newMessage);
+    }
+}
+
+console.log(repeatMessage(4, messagePlaceholder));
--- js/hw9.js
+++ js/hw9.js
@@ -1,66 +1,66 @@
-function logItems(array){
-    for (let i = 0; i < array.length; i++){
-        console.log(`${i + 1} = ${array[i]}`);
-    }
-}
-logItems(["Mango", "Poly", "Ajax"]);
+// function logItems(array){
+//     for (let i = 0; i < array.length; i++){
+//         console.log(`${i + 1} = ${array[i]}`);
+//     }
+// }
+// logItems(["Mango", "Poly", "Ajax"]);
 
-function calculatEngravingPrice(message, pricePerWord){
-const stringToArray = message.split(" ");
-    console.log(stringToArray.length * pricePerWord);
-}
-calculatEngravingPrice("Напиши скрипт підрахунку вартості гравіювання прикрас", 10);
+// function calculatEngravingPrice(message, pricePerWord){
+// const stringToArray = message.split(" ");
+//     console.log(stringToArray.length * pricePerWord);
+// }
+// calculatEngravingPrice("Напиши скрипт підрахунку вартості гравіювання прикрас", 10);
 
-function findLongestWord(string){
-    const convertedArray = string.split(" ");
-    let word = convertedArray[0];
-    for (let i = 0; i < convertedArray.length; i++){
-        if (word.lenght < convertedArray[i].length){
-            word = convertedArray[i];
-        }
-    }
-    console.log(word);
-}
-findLongestWord("Mango Poly Ajax");
+// function findLongestWord(string){
+//     const convertedArray = string.split(" ");
+//     let word = convertedArray[0];
+//     for (let i = 0; i < convertedArray.length; i++){
+//         if (word.lenght < convertedArray[i].length){
+//             word = convertedArray[i];
+//         }
+//     }
+//     console.log(word);
+// }
+// findLongestWord("Mango Poly Ajax");
 
-function formatString(string){
-    if (string.length <= 40){
-        return string
-    } else {
-        return string.slice(0, 41) + "..."
-    }
-}
-const message = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ducimus ratione cum harum quia, tempora minus pariatur nam natus totam neque doloremque maxime atque animi quis ab ipsa. Debitis, quia!";
-console.log(formatString(message));
+// function formatString(string){
+//     if (string.length <= 40){
+//         return string
+//     } else {
+//         return string.slice(0, 41) + "..."
+//     }
+// }
+// const message = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ducimus ratione cum harum quia, tempora minus pariatur nam natus totam neque doloremque maxime atque animi quis ab ipsa. Debitis, quia!";
+// console.log(formatString(message));
 
-function checkForSpam(message){
-    const lowerMessage = message.toLowerCase()
-    return lowerMessage.includes("spam") || lowerMessage.includes("sale")
-}
-const text = " hi hello sale"
-console.log(checkForSpam(text));
+// function checkForSpam(message){
+//     const lowerMessage = message.toLowerCase()
+//     return lowerMessage.includes("spam") || lowerMessage.includes("sale")
+// }
+// const text = " hi hello sale"
+// console.log(checkForSpam(text));
 
-let input;
-const numbers = [];
+// let input;
+// const numbers = [];
 
-while(true) {
-    input = prompt("Введіть число");
-    if (input === null){
-        alert("Ви нас покинули!");
-      break;
-    }
-    if (isNaN(input)){
-        alert("Введіть число!");
-        continue;
-    }
-    input = Number(input);
-    numbers.push(input);
-}
-if (numbers.length > 0){
-    let total = 0;
-    for (let i = 0; i < numbers.length; i++){
-        total += numbers[i];
-    }
-    console.log(total);    
-}
+// while(true) {
+//     input = prompt("Введіть число");
+//     if (input === null){
+//         alert("Ви нас покинули!");
+//       break;
+//     }
+//     if (isNaN(input)){
+//         alert("Введіть число!");
+//         continue;
+//     }
+//     input = Number(input);
+//     numbers.push(input);
+// }
+// if (numbers.length > 0){
+//     let total = 0;
+//     for (let i = 0; i < numbers.length; i++){
+//         total += numbers[i];
+//     }
+//     console.log(total);    
+// }
 
