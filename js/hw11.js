6b403298900ce77344bd6a67e84eaea33c6e87ec
Andrew87n <andlop@ukr.net>
2025-10-04 13:04:41 +0300

    123

--- js/hw11.js
+++ js/hw11.js
@@ -1,73 +1,75 @@
-// const bankAccount = {
-//     ownerName: "Max", 
-//     accountNumber: "012 345 67 89", 
-//     balance: 1000,
-//     deposit (cash) {
-//         alert(`На рахунку ${this.balance += cash} грн`);
-//     },
-//     withdraw (cash) {
-//         if (cash > this.balance){
-//              alert("Недостатньо грошей");
-//         }
-//         alert(`На рахунку ${this.balance -= cash} грн`);
-//     }
-// }
-// if (confirm("Ви хочете поповнити рахунок?")){
-//     const money = Number(prompt("Введіть суму для поповнення"));
-//     if (money > 0 && !isNaN(money)){
-//         bankAccount.deposit(money);
-//     } else {
-//         alert("Невірні дані")
-//     }
-// }
-// if (confirm("Бажаєте зняти кошти?")){
-//     const money = Number(prompt("Введіть суму яку хочете зняти"));
-//     if (money > 0 && !isNaN(money)){
-//         bankAccount.withdraw(money);
-//     } else {
-//         alert("Невірні дані")
-//     }
-// }
+const bankAccount = {
+    ownerName: "Max", 
+    accountNumber: "012 345 67 89", 
+    balance: 1000,
+    deposit (cash) {
+        alert(`На рахунку ${this.balance += cash} грн`);
+    },
+    withdraw (cash) {
+        if (cash > this.balance){
+             alert("Недостатньо грошей");
+        }
+        alert(`На рахунку ${this.balance -= cash} грн`);
+    }
+}
+if (confirm("Ви хочете поповнити рахунок?")){
+    const money = Number(prompt("Введіть суму для поповнення"));
+    if (money > 0 && !isNaN(money)){
+        bankAccount.deposit(money);
+    } else {
+        alert("Невірні дані")
+    }
+}
+if (confirm("Бажаєте зняти кошти?")){
+    const money = Number(prompt("Введіть суму яку хочете зняти"));
+    if (money > 0 && !isNaN(money)){
+        bankAccount.withdraw(money);
+    } else {
+        alert("Невірні дані")
+    }
+}
 
-// const weather = {
-//     temperature: Number(prompt("Введіть температуру")),
-//     humidity: "mild",
-//     windSpeed: "fast",
-//     isBelowZero () {
-//         if (this.temperature > 0){
-//             return alert("температура вище 0 градусів Цельсія") 
-//         } else {
-//             return alert("температура нижче 0 градусів Цельсія")
-//         }
-//     }
-// }
-// console.log(weather.isBelowZero());
+const weather = {
+    temperature: Number(prompt("Введіть температуру")),
+    humidity: "mild",
+    windSpeed: "fast",
+    isBelowZero () {
+        if (this.temperature > 0){
+            return alert("температура вище 0 градусів Цельсія") 
+        } else {
+            return alert("температура нижче 0 градусів Цельсія")
+        }
+    }
+}
+console.log(weather.isBelowZero());
 
-// const user = {
-//     name: "Max",
-//     email: "user@gmail",
-//     password: "Max123",
-//     login (email, password) {
-//         if (email === this.email && password === this.password){
-//             return alert("Вхід виконано")
-//         } else {
-//             return alert("Неправильно введені дані")
-//         }
-//     }
-// }
-// console.log(user.login("user@gmail", "Max123"));
+const user = {
+    name: "Max",
+    email: "user@gmail",
+    password: "Max123",
+    login (email, password) {
+        if(email === this.email && password === this.password){
+            console.log("Вхід виконано");
+            alert("Вхід виконано")
+        } else {
+            console.log("Сталася помилка");
+            alert("Сталася помилка")
+        }
+    }
+}
+user.login("user@gmail", "Max123")
 
-// const movie = {
-//     title: "Max movie",
-//     director: "Max",
-//     Year: "2026",
-//     Rating: 10,
-//     goodRating () {
-//         if (this.Rating >= 8){
-//             return true
-//         } else {
-//             return false
-//         }
-//     }
-// }
-// console.log(movie, movie.goodRating());
+const movie = {
+    title: "Max movie",
+    director: "Max",
+    Year: "2026",
+    Rating: 10,
+    goodRating () {
+        if (this.Rating >= 8){
+            return true
+        } else {
+            return false
+        }
+    }
+}
+console.log(movie, movie.goodRating());
